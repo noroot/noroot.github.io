f6f4f1cc0ba68c9663141caa42465b37f811d17a
dmr <noroot@falsetrue.io>
2024-06-21 15:07:05 +0400

    Update

---
+++ _bridgetown/static/index.42K5JGTD.js
@@ -0,0 +1,16 @@
+(() => {
+  // frontend/javascript/index.js
+  document.addEventListener("DOMContentLoaded", function() {
+    var e = "cHVibGljQGZhbHNldHJ1ZS5pbwo=";
+    const ee = ["em_s"];
+    var f = "";
+    for (i = 0; i <= ee.length - 1; i++) {
+      v = ee[i];
+      els = document.getElementsByClassName(v);
+      for (y = 0; y <= els.length - 1; y++) {
+        els[y].setAttribute("href", "mailto:".concat(atob(e)));
+      }
+    }
+  });
+})();
+//# sourceMappingURL=/_bridgetown/static/index.42K5JGTD.js.map
---
+++ legacy/slides/README.md
@@ -0,0 +1,1 @@
+# slides.falsetrue.io
